51f9116f2f70f629ac8fe50fa4378eb342c1363b
Sebastian Sanchez <ssanchez@bassan.co>
2015-03-22 21:24:42 -0500

    90862272 Validaciones csv

--- src/main/java/co/bassan/lectora/core/EjecutorValidaciones.java
+++ src/main/java/co/bassan/lectora/core/EjecutorValidaciones.java
@@ -22,7 +22,11 @@ public class EjecutorValidaciones {
 
     private static EjecutorValidaciones intancia;
 
-
+    /**
+     * Metodo encargado de ejecutar las validaciones basicas de un archivo
+     *
+     * @return
+     */
     public List<ErrorCampo> ejecutor() {
         List<ErrorCampo> errores = new ArrayList<ErrorCampo>();
         try {
@@ -52,6 +56,14 @@ public class EjecutorValidaciones {
         return errores;
     }
 
+    /**
+     * valida el formato de fecha
+     *
+     * @param formatoFecha
+     * @param valor
+     * @param errores
+     * @param fila
+     */
     private void validarFormatoFecha(String formatoFecha, String valor, List<ErrorCampo> errores, int fila) {
 
         SimpleDateFormat dateFormat = new SimpleDateFormat(formatoFecha);
@@ -70,56 +82,121 @@ public class EjecutorValidaciones {
 
     }
 
+    /**
+     * valida que este en los valores opcionales
+     * @param listaLimitante
+     * @param valor
+     * @param errores
+     * @param fila
+     */
     private void validarListaLimite(String[] listaLimitante, String valor, List<ErrorCampo> errores, int fila) {
         if (!Arrays.asList(listaLimitante).contains(valor))
             agregarError("No esta entre los valores posibles", valor, errores, fila);
     }
 
+    /**
+     * validaciones generales del docuneto
+     *
+     * Validar cantidad de campos obligatorios
+     * @throws CargueCsvExcepcion
+     */
     private void validacionesGenerales() throws CargueCsvExcepcion {
         if (config.getCantidadCampos() != cantidadCarga) {
             throw new CargueCsvExcepcion("Numero de columanas diferentes", null, fila);
         }
     }
 
+    /**
+     * validacion de patrones
+     * @param exprecion
+     * @param valor
+     * @param errores
+     * @param fila
+     */
     private void validarPatron(String exprecion, String valor, List<ErrorCampo> errores, int fila) {
         if (valor.matches(exprecion)) {
             agregarError("No tiene el formato adecuado", valor, errores, fila);
         }
     }
 
+    /**
+     * validacion de longitud maxima
+     * @param longitudMaxima
+     * @param valor
+     * @param errores
+     * @param fila
+     */
     private void validarLongitudMaxima(int longitudMaxima, String valor, List<ErrorCampo> errores, int fila) {
         if (valor.length() > longitudMaxima) {
             agregarError("No tiene la longitud maxima", valor, errores, fila);
         }
     }
 
+    /**
+     * validacion de longitud minima
+     * @param longitudMinima
+     * @param valor
+     * @param errores
+     * @param fila
+     */
     private void validarLongitudMinima(int longitudMinima, String valor, List<ErrorCampo> errores, int fila) {
         if (valor.length() < longitudMinima) {
             agregarError("No tiene la longitud minima", valor, errores, fila);
         }
     }
 
+    /**
+     * Validar si el campo es requerido
+     * @param valor
+     * @param fila
+     * @throws CargueCsvExcepcion
+     */
     public void validarSiEsRequerido(String valor, int fila) throws CargueCsvExcepcion {
         if (valor == null || "".equals(valor.trim())) {
             throw new CargueCsvExcepcion("el valor es requerido: " + valor, valor, fila);
         }
     }
 
+    /**
+     * Agrega errores al acumulado de errores del archivo
+     * @param causa
+     * @param valor
+     * @param errores
+     * @param fila
+     */
     private void agregarError(String causa, String valor, List<ErrorCampo> errores, int fila) {
         agregarError(new ErrorCampo(causa, valor, fila), errores);
     }
 
+    /**
+     * Agrega errores al acumulado de errores del archivo
+     * @param error
+     * @param errores
+     */
     private void agregarError(ErrorCampo error, List<ErrorCampo> errores) {
         error.setFila(fila);
         errores.add(error);
     }
 
+    /**
+     * Constructor
+     * @param config
+     * @param fila
+     * @param cantidadCarga
+     */
     public EjecutorValidaciones(ConfiguracionCarga config, int fila, int cantidadCarga) {
         this.config = config;
         this.fila = fila;
         this.cantidadCarga = cantidadCarga;
     }
 
+    /**
+     * Singleton
+     * @param configCampo
+     * @param fila
+     * @param cantidadCargada
+     * @return
+     */
     public static EjecutorValidaciones getIntancia(ConfiguracionCarga configCampo, int fila, int cantidadCargada) {
 
         if (intancia == null) {
@@ -130,6 +207,12 @@ public class EjecutorValidaciones {
         }
     }
 
+    /**
+     * Modificacion de datos de la instancia
+     * @param config
+     * @param fila
+     * @param cantidadCarga
+     */
     private void modificarDatos(ConfiguracionCarga config, int fila, int cantidadCarga) {
         this.config = config;
         this.fila = fila;
